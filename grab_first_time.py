61e2f89b1cd8debec5599289a1d2c76529ce9d8d
David Hahn <hahnx117@umn.edu>
2022-07-19 16:09:06 -0500

    add come commented code

--- grab_first_time.py
+++ grab_first_time.py
@@ -4,6 +4,13 @@ from bs4 import BeautifulSoup
 import json
 import datetime
 
+## First, grab the object to get the hourly forecast URL from
+# baseurl = https://api.weather.gov
+# response = requests.get(f'{baseurl}/points/44.981944,-93.269167')
+
+## Assign the returned value as the new URL
+# url = response.json()['properties']['forecastHourly']
+# response = requests.get(url)
 
 baseurl = "https://www.ncei.noaa.gov/cdo-web/api/v2"
 url = 'https://api.weather.gov/gridpoints/MPX/107,71/forecast/hourly'
