520d8aa971fb2512bad87e1a656924d8c4c321dc
Daniel <eric2952@umn.edu>
2022-09-17 14:04:47 -0500

    Added loop to enable requesting multiple times before user exits

--- grab_first_time.py
+++ grab_first_time.py
@@ -13,30 +13,33 @@ import datetime
 # response = requests.get(url)
 
 #assign sunset datetime object
-requesteddate = input('Please enter the date you would like to retrieve temperatures for (YYYY-MM-DD): ')
-baseurl2 = 'http://api.sunrise-sunset.org/json'
-data = {'lat':44.9778,'lng':'-93.2650','date':requesteddate}
-response2=requests.get(baseurl2,params=data)
-mspdusk = response2.json()['results']['sunset']
-mspduskdate = datetime.datetime.strptime(f'{requesteddate} {mspdusk}',"%Y-%m-%d %H:%M:%S %p") - datetime.timedelta(hours=5) + datetime.timedelta(days=1)
+#loop until user inputs 'exit' command
+requesteddate = input('Please enter the date you would like to retrieve temperatures for (YYYY-MM-DD), or type "exit" to leave: ')
+while requesteddate != "exit":
+    
+    baseurl2 = 'http://api.sunrise-sunset.org/json'
+    data = {'lat':44.9778,'lng':'-93.2650','date':requesteddate}
+    response2=requests.get(baseurl2,params=data)
+    mspdusk = response2.json()['results']['sunset']
+    mspduskdate = datetime.datetime.strptime(f'{requesteddate} {mspdusk}',"%Y-%m-%d %H:%M:%S %p") - datetime.timedelta(hours=5) + datetime.timedelta(days=1)
 
-#assign temperature object
-baseurl = "https://www.ncei.noaa.gov/cdo-web/api/v2"
-url = 'https://api.weather.gov/gridpoints/MPX/107,71/forecast/hourly'
+    #assign temperature object
+    baseurl = "https://www.ncei.noaa.gov/cdo-web/api/v2"
+    url = 'https://api.weather.gov/gridpoints/MPX/107,71/forecast/hourly'
 
 
-response=requests.get(url)
+    response=requests.get(url)
 
-#grabbing the temperature - nowstemp - and the temperature unit -tempunit- from noaa
-#assigning nowstime as a datetime object
-nowstemp = response.json()["properties"]["periods"][0]["temperature"]
-nowstempunit = response.json()["properties"]["periods"][0]["temperatureUnit"]
-nowstime=datetime.datetime.now().strftime("%I:%M %p, %B %d, %Y")
-#nowsdusk=response2.json()["results"]["sunset"]
+    #grabbing the temperature - nowstemp - and the temperature unit -tempunit- from noaa
+    #assigning nowstime as a datetime object
+    nowstemp = response.json()["properties"]["periods"][0]["temperature"]
+    nowstempunit = response.json()["properties"]["periods"][0]["temperatureUnit"]
+    nowstime=datetime.datetime.now().strftime("%I:%M %p, %B %d, %Y")
+    #nowsdusk=response2.json()["results"]["sunset"]
 
-#print out "The temperature is: nowstemp nowstempunit - nowstime"
-print(f'The dusk time on {requesteddate} is {mspduskdate}')
-print ("The temperature is: " + str(nowstemp)+ " " + str(nowstempunit)+" - "+str(nowstime) )
-#print ("Dusk today is at "+nowsdusk)
-
+    #print out "The temperature is: nowstemp nowstempunit - nowstime"
+    print(f'The dusk time on {requesteddate} is {mspduskdate}')
+    print ("The temperature is: " + str(nowstemp)+ " " + str(nowstempunit)+" - "+str(nowstime) )
+    #print ("Dusk today is at "+nowsdusk)
+    requesteddate = input('Please enter the date you would like to retrieve temperatures for (YYYY-MM-DD), or type "exit" to leave: ')
 
